f05cf283ab2d9ea44e56b6982c2a74d4d64b1ce3
calm <pingjingsht@163.com>
2018-09-13 17:23:34 +0800

    3-6

--- task_antd/src/admin.js
+++ task_antd/src/admin.js
@@ -2,17 +2,22 @@ import React from 'react'
 import {Row,Col} from 'antd';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import NavLeft from './components/NavLeft';
+import './style/common.less';
 export default class Admin extends React.Component{
     render(){
         return (
-            <Row>
-                <Col span="3">
-                    Left
+            <Row className="container">
+                <Col span="3" className="nav-left">
+                    <NavLeft/>
                 </Col>
-                <Col span="21">
-                    <Header>header</Header>
-                    <Row>context</Row>
-                    <Footer>Footer</Footer>
+                <Col span="21" className="main">
+                    <Header/>
+                    <Row className="content">
+                        content
+                        {/*this.props.children*/}
+                    </Row>
+                    <Footer/>
                 </Col>
             </Row>
         );
